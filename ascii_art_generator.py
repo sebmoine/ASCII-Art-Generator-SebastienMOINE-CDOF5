9a411575d9fad3a0113f884526fdc60f84f0003c
race <96184147+raacee@users.noreply.github.com>
2024-01-29 18:22:08 +0100

    Update ascii_art_generator.py

--- ascii_art_generator.py
+++ ascii_art_generator.py
@@ -1,13 +1,15 @@
 import pyfiglet
 
-def generate_ascii_art(text):
+def generate_ascii_art(text, fontsize=None):
     # Using pyfiglet for ASCII art conversion
-    ascii_art = pyfiglet.figlet_format(text)
+    ascii_art = pyfiglet.figlet_format(text, width=fontsize)
     return ascii_art
 
 def main():
     user_input = input("Enter text for ASCII art: ")
-    art = generate_ascii_art(user_input)
+    font_size_input = input("Enter font size : ")
+    
+    art = generate_ascii_art(user_input, int(font_size_input))
     print(art)
 
 if __name__ == "__main__":
