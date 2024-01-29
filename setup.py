382300f502d348057aa5489661a49381cfbc0c94
sebmoine <114504523+sebmoine@users.noreply.github.com>
2024-01-29 17:41:07 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='ASCII-Art-Generator',
+ version='1.0',
+ author='sebmoine',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
